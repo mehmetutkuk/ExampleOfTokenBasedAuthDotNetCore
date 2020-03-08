f0422848dd3302a669e36ef762feb33da6ac7c7d
M. UTKU KUL <m.utkukul@gmail.com>
2020-03-08 16:12:24 +0300

    This is fake user management service.
    I don't connect any kind of db's.
    This part changes which data layer used.

---
+++ ExampleOfTokenBasedAuthDotNetCore/UserManagementService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleOfTokenBasedAuthDotNetCore
+{
+    public class UserManagementService
+    {
+
+        public bool IsValidUser(string userName, string password)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
