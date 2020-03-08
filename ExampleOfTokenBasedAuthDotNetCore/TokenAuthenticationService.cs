36be86e5da189512a17cbf88a070f007c3901c18
M. UTKU KUL <m.utkukul@gmail.com>
2020-03-08 15:47:11 +0300

    Generate Interface And Add Nuget Package In It

--- ExampleOfTokenBasedAuthDotNetCore/TokenAuthenticationService.cs
+++ ExampleOfTokenBasedAuthDotNetCore/TokenAuthenticationService.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityModel.Client;
 
 namespace ExampleOfTokenBasedAuthDotNetCore
 {
-    public class TokenAuthenticationService
+    public class TokenAuthenticationService 
     {
+
+    }
+
+    public interface IAuthenticateService
+    {
+        bool IsAuthenticated(TokenRequest request, out string token);
     }
 }
